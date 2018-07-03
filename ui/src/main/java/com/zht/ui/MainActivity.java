cb6ef8a577f4f8ae971938f8b4fe7bd666cefc28
zhanghaitao1 <zhanghaitao1@longfor.com>
2018-07-03 14:34:38 +0800

    添加数据库框架的module

--- ui/src/main/java/com/zht/ui/MainActivity.java
+++ ui/src/main/java/com/zht/ui/MainActivity.java
@@ -2,15 +2,20 @@ package com.zht.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.zht.ui.filter.MyView;
+import com.zht.ui.revealview.RevealDrawble;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        ImageView imageView = new ImageView(this);
+        setContentView(imageView);
 
+        RevealDrawble rd = new RevealDrawble(getResources().getDrawable(R.drawable.avft),getResources().getDrawable(R.drawable.avft_active));
+        imageView.setBackground(rd);
     }
 }
--- ui/src/main/java/com/zht/ui/revealview/RevealDrawble.java
+++ ui/src/main/java/com/zht/ui/revealview/RevealDrawble.java
@@ -2,14 +2,64 @@ package com.zht.ui.revealview;
 
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.PixelCopy;
 
 public class RevealDrawble extends Drawable{
+    private final Rect mRect = new Rect();
+    private Drawable mUnselectedDrawable;
+    private Drawable mSelectedDrawble;
+
+    public RevealDrawble(Drawable mUnselectedDrawable, Drawable mSelectedDrawble) {
+        this.mUnselectedDrawable = mUnselectedDrawable;
+        this.mSelectedDrawble = mSelectedDrawble;
+
+        setLevel(100);
+    }
+
     @Override
     public void draw(@NonNull Canvas canvas) {
+        Rect r = getBounds();
+        Rect temp = new Rect();
+        //抠图
+        Gravity.apply(Gravity.LEFT,r.width()/2,r.height(),r,temp);
 
+        canvas.save();
+        canvas.clipRect(temp);
+        mUnselectedDrawable.draw(canvas);
+        canvas.restore();
+
+        Gravity.apply(Gravity.RIGHT,r.width()/2,r.height(),r,temp);
+        canvas.clipRect(temp);
+        mSelectedDrawble.draw(canvas);
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        super.onBoundsChange(bounds);
+        mUnselectedDrawable.setBounds(bounds);
+        mSelectedDrawble.setBounds(bounds);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return Math.max(mSelectedDrawble.getIntrinsicWidth(),mUnselectedDrawable.getIntrinsicWidth());
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return Math.max(mSelectedDrawble.getIntrinsicHeight(),mUnselectedDrawable.getIntrinsicHeight());
+    }
+
+    @Override
+    protected boolean onLevelChange(int level) {
+        invalidateSelf();
+        return true;
     }
 
     @Override
@@ -24,6 +74,6 @@ public class RevealDrawble extends Drawable{
 
     @Override
     public int getOpacity() {
-        return 0;
+        return PixelFormat.UNKNOWN;
     }
 }
